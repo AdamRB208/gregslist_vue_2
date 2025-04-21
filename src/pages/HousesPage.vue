fffdb7c64f98522b70f746d47ce49e36421bbb78
AdamRB208 <adamrborup@gmail.com>
2025-04-20 23:16:50 -0600

    styled houses page

--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -4,8 +4,34 @@
 
 
 <template>
-  <div>Houses here</div>
+  <section class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="text-center">
+          <h1 class="display-3">Houses</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row">
+      <div class="col-md-6">
+        Form here
+      </div>
+      <div class="col-md-6">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1680300960814-f684429a9cad?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fGhhcHB5JTIwcGVvcGxlJTIwaW4lMjBhJTIwaG91c2V8ZW58MHwxfDB8fHww"
+          alt="image of a couple moving into a new house">
+      </div>
+    </div>
+  </section>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+  max-height: 80dvh;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
